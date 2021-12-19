9b1d8ae6330f2a2682f06c90905062917af6aac6
Nathan Page <nathan.page@credosemi.com>
2021-12-18 17:28:49 -0800

    remove playground scripts from tracking

--- test/create2.lua
+++
@@ -1,8 +0,0 @@
-local fort = require "fort"
-local inspect = require "inspect"
-
-local a = fort.create_table()
-
-fort.table_write(a, {{[3] = "2", [5] = "3", test = "4"}, {"a", "b", "c", "d"}})
-
-print(fort.to_string(a))
