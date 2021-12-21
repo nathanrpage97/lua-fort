d656e68e24d7fa679292db7f1538c289748bd289
Nathan Page <nathanrpage97@gmail.com>
2021-12-21 12:16:58 -0500

    remove unused assert arg

--- src/fort.lua
+++ src/fort.lua
@@ -4,31 +4,6 @@ local cfort = require "cfort"
 
 local fort = {}
 
---- assert that the given argument is in fact of the correct type.
--- @param n argument index
--- @param val the value
--- @param tp the type
--- @param verify an optional verification function
--- @param msg an optional custom message
--- @param lev optional stack position for trace, default 2
--- @return the validated value
--- @raise if `val` is not the correct type
--- @usage
--- local param1 = assert_arg(1,"hello",'table')  --> error: argument 1 expected a 'table', got a 'string'
--- local param4 = assert_arg(4,'!@#$%^&*','string',path.isdir,'not a directory')
---      --> error: argument 4: '!@#$%^&*' not a directory
-local function assert_arg(n, val, tp, verify, msg, lev)
-    if type(val) ~= tp then
-        error(("argument %d expected a '%s', got a '%s'"):format(n, tp,
-                                                                 type(val)),
-              lev or 2)
-    end
-    if verify and not verify(val) then
-        error(("argument %d: '%s' %s"):format(n, val, msg), lev or 2)
-    end
-    return val
-end
-
 local function split(inputstr, sep)
     if sep == nil then sep = "%s" end
     local t = {}
