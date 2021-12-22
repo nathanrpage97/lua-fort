324d97558e54597368e19f62751d555ec0d0cbc3
Nathan Page <nathanrpage97@gmail.com>
2021-12-22 10:02:24 -0500

    add inspect to install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 set -x -e
 
-# all of the packages needed for development
+# all of the packages for development
 
+# testing
 luarocks install busted
-luarocks install lua-discount
-luarocks install --server=https://luarocks.org/dev luaformatter
+
+# docs
 luarocks install ldoc
+luarocks install lua-discount
+
+# linting / formatting
+luarocks install --server=https://luarocks.org/dev luaformatter
 luarocks install luacheck
+
+# utility
+luarocks install inspect
