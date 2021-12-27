c9ebf1676db26d96fddda2b58d8a3a8a56a92be5
Nathan Page <nathanrpage97@gmail.com>
2021-12-27 14:29:17 -0500

    group fort methods docs

--- examples/1-simple-table.lua
+++ examples/1-simple-table.lua
@@ -3,7 +3,7 @@ local ft = require "fort"
 local ftable = ft.create_table()
 
 -- setup header
-ft.set_cell_prop(ftable, 1, ft.ANY_COLUMN, ft.CPROP_ROW_TYPE, ft.ROW_HEADER)
+ftable.set_cell_prop(ftable, 1, ft.ANY_COLUMN, ft.CPROP_ROW_TYPE, ft.ROW_HEADER)
 ft.write_ln(ftable, "N", "Driver", "Time", "Avg Speed")
 ft.write_ln(ftable, "1", "Ricciardo", "1:25.945", "222.128")
 ft.write_ln(ftable, "2", "Hamilton", "1:26.373", "221.027")
